12de2a57916b80873e8615396a2985d8823f3eb1
Angela Wolff <warriorwomenblog@gmail.com>
2019-04-11 20:40:32 +0100

    Connecting to API via query string in url Currently performing a fetch in app.js via a form Need to strip this out into its own component Also should explore use of proxy

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,31 @@ import Inset from "./components/Inset";
 import Starwars from "./components/Starwars";
 
 class App extends Component {
-  state = {};
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      greeting: ""
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ name: event.target.value });
+  }
+
+  handleSubmit = async e => {
+    e.preventDefault();
+    console.log("Submitted!");
+    const response = await fetch(
+      `http://localhost:4000/api/greeting?name=${encodeURIComponent(
+        this.state.name
+      )}`
+    );
+    const body = await response.json();
+    this.setState({ greeting: body.greeting });
+  };
 
   componentDidMount() {}
 
@@ -28,6 +52,17 @@ class App extends Component {
         <div className="box i" />
         <div className="box inset">
           <Inset />
+          <form onSubmit={this.handleSubmit}>
+            <label htmlFor="name">Enter your name: </label>
+            <input
+              id="name"
+              type="text"
+              value={this.state.name}
+              onChange={this.handleChange}
+            />
+            <button type="submit">Submit</button>
+          </form>
+          <p>{this.state.greeting}</p>
         </div>
       </div>
     );
--- server.js
+++ server.js
@@ -1,32 +1,43 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const pino = require("express-pino-logger")();
+const cors = require("cors");
 
 const app = express();
-const port = process.env.PORT || 5000;
-
+const port = process.env.PORT || 4000;
+// app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(pino);
+app.use(cors());
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, "client/build")));
 
 // API Calls
-app.get('/api/hello', (req,res) => {
-  res.send({ express: 'Hello from the express server'});
+// Obligatory Hello World!
+app.get("/api/greeting", (req, res) => {
+  const name = req.query.name || "World";
+  res.setHeader("Content-Type", "application/json");
+  res.send(JSON.stringify({ greeting: `Hello ${name}!` }));
 });
 
-app.post('/api/world', (req, res) => {
+app.get("/api/hello", (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  res.send({ express: "Hello from the express server" });
+});
+
+app.post("/api/world", (req, res) => {
   console.log(req.body);
   res.send(
-    `I received your POST request. This is what you sent me: ${req.body.post}`,
+    `I received your POST request. This is what you sent me: ${req.body.post}`
   );
 });
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
